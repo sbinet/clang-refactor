9b47e762f1d501353913d695b4db6f15d259e5d8
Sebastien Binet <binet@cern.ch>
2013-03-19 10:27:51 +0100

    test-classrename: update for hwaf

--- tests/ClassRename/test.sh
+++ tests/ClassRename/test.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-cp foo.orig.h foo.h
-cp foo.orig.cpp foo.cpp
-clang -c foo.cpp
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:STRING=ON .
-make
 
-mkdir -p ../../Build
-cd ../../Build/
-cmake ../
-make
-cd -
+set -e
 
-../../Build/refactorial < test.yml
+/bin/cp foo.orig.h foo.h
+/bin/cp foo.orig.cpp foo.cpp
+cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:STRING=ON \
+    .
+#sed -e 's@clang++@g++@g' -i compile_commands.json 
+make
+make rebuild_cache
+make clean
+
+#/bin/rm -rf compile_commands.json; touch compile_commands.json
+set +e
+clang-refactorial < test.yml
+
+diff -urN foo.orig.h foo.h
+diff -urN foo.orig.cpp foo.cpp
+
 touch foo.h foo.cpp
 make
