41264b421bbf44e1af3060ebda5b8448790083cf
Sebastien Binet <binet@cern.ch>
2013-03-19 10:39:52 +0100

    test-decltyperename: update for hwaf

--- tests/DeclTypeRename/test.sh
+++ tests/DeclTypeRename/test.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-cp foo.orig.h foo.h
-cp foo.orig.cpp foo.cpp
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:STRING=ON .
-make
 
-mkdir -p ../../Build
-cd ../../Build/
-cmake ../
-make
-cd -
+set -e
 
-../../Build/refactorial < test.yml
+/bin/cp foo.orig.h foo.h
+/bin/cp foo.orig.cpp foo.cpp
+cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:STRING=ON \
+    .
+make
+make rebuild_cache
+make clean
+
+clang-refactorial < test.yml
+
+set +e
+diff -urN foo.orig.h foo.h
+diff -urN foo.orig.cpp foo.cpp
+
+set -e
 touch foo.h foo.cpp
 make
